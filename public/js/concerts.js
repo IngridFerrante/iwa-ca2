aba3116c199f55dc18d63229129dbea9b8724a2d
IngridFerrante <ferrantesingrid@gmail.com>
2022-05-24 17:13:16 +0000

    new js functions

--- public/js/concerts.js
+++ public/js/concerts.js
@@ -1,3 +1,5 @@
+let concertSelected='';//global variable to know what recipe is selected
+let concertIds=[];
 
 function addConcert(){
     $("#form-concerts").empty();
@@ -12,4 +14,71 @@ function addConcert(){
     "</form>"
     $("#form-concerts").append(strHtml);
 }
+
+//saving the concert that was submitted via API
+function submitConcert() {
+    var jsonToAdd = getFields();
+    let obj = JSON.parse(jsonToAdd)
+    console.log(jsonToAdd)
+
+    
+    if(jsonToAdd == null){
+        alert("Fill all the fields");
+        return;
+    }
+    
+    let newId = '';
+    
+    fetch(apiURL,{
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(obj)
+    }).then(response => response.json()) 
+    .then(json => {
+        console.log(json)
+        newId =json._id ;
+    })
+    .catch(err => console.log(err));
+    
+    
+    getNames(); //getTitles
+    $("#form-concerts").empty();
+    concertSelected='';
+    
+}
+
+// take all the information and parses it to a JSON Format
+function getFields(){
+
+    var location = document.getElementById("clocation").value;
+    var concertName = document.getElementById("cname").value.trim();
+
+    
+    var strJSON = '{'+
+        
+        '"Concert":"' + concertName ;
+
+
+    if(strJSON.endsWith(',')){
+        strJSON = strJSON.slice(0,strJSON.length -1);
+    }
+
+    strJSON+='],'+
+        '"location":"'+location+'"}';
+
+
+        //null if there is any field empty
+    if(concertName == '' ||location == ''){  
+        return null;
+    }
+    else{
+        console.log(strJSON);
+        return strJSON.replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
+        
+    }
+
+}
+
 
